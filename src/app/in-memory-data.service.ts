b297907b6d8c99fd9ebe216d39e521db71eacf7c
VladimirJotov <v.jotov@gmail.com>
2017-12-03 15:15:49 +0700

    update book names

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const bibItems = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 11, name: 'One Station Away: A Novel ' },
+      { id: 12, name: 'SuperCell: Critical Incident #1' },
+      { id: 13, name: 'Dawn of the Hunter - An Action Thriller Novel' },
+      { id: 14, name: 'Dawn Girl: A Gripping Serial Killer Thriller' },
+      { id: 15, name: 'Glimpse of Death: A Riveting Serial Killer Thriller' },
+      { id: 16, name: 'First Strike' },
+      { id: 17, name: 'Year One: Chronicles of the One, Book 1' },
+      { id: 18, name: 'Good Guys Love Dogs' },
+      { id: 19, name: 'Winter Wishes' },
+      { id: 20, name: 'Sugarplum Way' }
     ];
     return {bibItems};
   }
